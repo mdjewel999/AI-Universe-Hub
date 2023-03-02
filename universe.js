f8c4d5621d6f0f39a6938593df8253357fdd6875
mdjewel999 <mdjewel878761@gmail.com>
2023-03-02 21:32:05 +0600

    first header section

---
+++ universe.js
@@ -0,0 +1,7 @@
+const fetchCategories = () => {
+    fetch("https://openapi.programming-hero.com/api/ai/tools")
+      .then((res) => res.json())
+      .then((data) => console.log(data.data));
+  };
+  
+  fetchCategories()
