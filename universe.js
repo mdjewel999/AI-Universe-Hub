3f03c8ace34da1af25adca73785563e291983921
mdjewel999 <mdjewel878761@gmail.com>
2023-03-03 02:31:54 +0600

    loaduniverses fatech data url

--- universe.js
+++ universe.js
@@ -1,7 +1,82 @@
-const fetchCategories = () => {
-    fetch("https://openapi.programming-hero.com/api/ai/tools")
-      .then((res) => res.json())
-      .then((data) => console.log(data.data));
+const loadUniverse = async () => {
+  const url = `https://openapi.programming-hero.com/api/ai/tools`;
+  const res = await fetch(url);
+  const data = await res.json();
+  displayUniverse(data.data.tools);
+};
+
+const displayUniverse = universes => {
+    // console.log(universes);
+  const universeContainer = document.getElementById("universe_container");
+  universes.forEach(universe => {
+    const universeDiv = document.createElement("div");
+    universeDiv.classList.add("col");
+    universeDiv.innerHTML = `
+    <div class="card h-100">
+    <img src="${universe.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${universe.description ? universe.description : " "}</h5>
+        <p class="card-text">1. ${universe.features[0]}</p>
+        <p class="card-text">2. ${universe.features[1]}</p>
+        <p class="card-text">3. ${universe.features[2]}</p>
+        <hr>
+       <div  class="d-flex justify-content-between align-items-center">
+          <div>
+             <h3 class="card-text mt-2">${universe.name}</h3>
+         <div class="d-flex align-items-center gap-2">
+            <i class="fa-solid fa-calendar-days"></i> 
+            <p class="card-text">${ universe.published_in}</p>
+          </div>
+         </div>
+
+         <div class="mt-4">
+         <i class="fas fa-arrow-right text-danger" onclick="fetchNewsDetail()" data-bs-toggle="modal"
+         data-bs-target="#exampleModal"></i>
+         </div>
+       </div>
+
+      </div>
+    </div>
+
+    `;
+universeContainer.appendChild(universeDiv)
+  });
+};
+
+
+
+/* ******************************** */
+
+
+
+const fetchNewsDetails = async () => {
+    const url = `https://openapi.programming-hero.com/api/ai/tool/01`;
+    console.log(url)
+    const res = await fetch(url);
+    const data = await res.json();
+    displayUniverse2(data.data.tools);
   };
   
-  fetchCategories()
+  const displayUniverse2 = universes2 => {
+      // console.log(universes);
+    const universeContainer = document.getElementById("universe_container");
+    universes2.forEach(universe2 => {
+      const universeDiv = document.createElement("div");
+      universeDiv.classList.add("col");
+      universeDiv.innerHTML = `
+      <div class="card h-100">
+      <img src="${universe2.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+          <h5 class="card-title">${universe2.description}</h5>
+
+  
+        </div>
+      </div>
+  
+      `;
+  universeContainer.appendChild(universeDiv)
+    });
+  };
+
+  /* ***************** */
+loadUniverse();
